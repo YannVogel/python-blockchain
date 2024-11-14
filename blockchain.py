a010ace645845a9dd358b8ae633994aecf539a9f
Yann <yann@offroadlabs.com>
2024-11-14 09:37:12 +0100

    end of section

--- blockchain.py
+++ blockchain.py
@@ -4,15 +4,19 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ The get_last_blockchain_value function returns the last value of the current blockchain."""
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_value(transaction_amount, last_transaction):
     """ The add_value function appends a new block to the blockchain.
     Arguments:
         :transaction_amount: The amount that should be added to the blockchain.
         :last_transaction: The last blockchain transaction (default [1]).
     """
+    if last_transaction is None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -29,15 +33,30 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print('-' * 20)
 
 
-tx_amount = get_transaction_value()
-add_value(tx_amount)
+def verify_chain():
+    is_valid = True
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        if blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+    return is_valid
 
-while True:
+
+waiting_for_input = True
+
+while waiting_for_input:
     print('Please choose: ')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
@@ -45,9 +64,16 @@ while True:
         add_value(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
+    if not verify_chain():
+        print_blockchain_elements()
+        print('Invalid blockchain!')
+        break
 
 print('Done!')
