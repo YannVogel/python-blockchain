2484b0eef7d877595d6b0852cf4d9c4707a3b3e2
Yann <yann@offroadlabs.com>
2024-11-20 15:50:41 +0100

    end of section

--- utility/verification.py
+++ utility/verification.py
@@ -27,7 +27,7 @@ class Verification:
     @staticmethod
     def verify_transaction(transaction, get_balance, check_funds=True):
         if check_funds:
-            sender_balance = get_balance()
+            sender_balance = get_balance(transaction)
             return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
         return Wallet.verify_transaction(transaction)
 
--- wallet.py
+++ wallet.py
@@ -7,19 +7,24 @@ from Crypto.Signature import PKCS1_v1_5
 
 
 class Wallet:
-    def __init__(self):
+    """Creates, loads and holds private and public keys. Manages transaction signing and verification."""
+
+    def __init__(self, node_id):
         self.private_key = None
         self.public_key = None
+        self.node_id = node_id
 
     def create_keys(self):
-        private_key, public_key = self.__generate_keys()
+        """Create a new pair of private and public keys."""
+        private_key, public_key = self.generate_keys()
         self.private_key = private_key
         self.public_key = public_key
 
     def save_keys(self):
+        """Saves the keys to a file (wallet.txt)."""
         if self.public_key is not None and self.private_key is not None:
             try:
-                with open('wallet.txt', mode='w') as f:
+                with open('wallet-{}.txt'.format(self.node_id), mode='w') as f:
                     f.write(self.public_key)
                     f.write('\n')
                     f.write(self.private_key)
@@ -29,24 +34,35 @@ class Wallet:
                 return False
 
     def load_keys(self):
+        """Loads the keys from the wallet.txt file into memory."""
         try:
-            with open('wallet.txt', mode='r') as f:
+            with open('wallet-{}.txt'.format(self.node_id), mode='r') as f:
                 keys = f.readlines()
-                self.public_key = keys[0][:-1]
-                self.private_key = keys[1]
-                return True
+                public_key = keys[0][:-1]
+                private_key = keys[1]
+                self.public_key = public_key
+                self.private_key = private_key
+            return True
         except (IOError, IndexError):
             print('Loading wallet failed...')
             return False
 
     @staticmethod
-    def __generate_keys():
+    def generate_keys():
+        """Generate a new pair of private and public key."""
         private_key = RSA.generate(1024, Crypto.Random.new().read)
         public_key = private_key.publickey()
         return (binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'),
-                binascii.hexlify(public_key.export_key(format='DER')).decode('ascii'))
+                binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii'))
 
     def sign_transaction(self, sender, recipient, amount):
+        """Sign a transaction and return the signature.
+
+        Arguments:
+            :sender: The sender of the transaction.
+            :recipient: The recipient of the transaction.
+            :amount: The amount of the transaction.
+        """
         signer = PKCS1_v1_5.new(RSA.importKey(binascii.unhexlify(self.private_key)))
         h = SHA256.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
         signature = signer.sign(h)
@@ -54,6 +70,11 @@ class Wallet:
 
     @staticmethod
     def verify_transaction(transaction):
+        """Verify the signature of a transaction.
+
+        Arguments:
+            :transaction: The transaction that should be verified.
+        """
         public_key = RSA.importKey(binascii.unhexlify(transaction.sender))
         verifier = PKCS1_v1_5.new(public_key)
         h = SHA256.new((str(transaction.sender) + str(transaction.recipient) + str(transaction.amount)).encode('utf8'))
